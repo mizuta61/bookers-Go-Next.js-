325a0fafb3a42d35e0a7a5615135600e0cb34f57
yutomizuta <yuto11752@i.softbank.jp>
2022-09-25 01:38:20 +0900

    env_file

--- api/main.go
+++ api/main.go
@@ -5,21 +5,15 @@ import (
 	"api/ent/migrate"
 	"api/models"
 	"context"
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		fmt.Printf("読み込み出来ませんでした: %v", err)
-	}
 	front_url := os.Getenv("FRONT_URL")
 
 	r := gin.Default()
@@ -31,7 +25,7 @@ func main() {
 
 	client := db.OpenMariadb()
 	defer client.Close()
-	err = client.Schema.Create(
+	err := client.Schema.Create(
 		ctx,
 		migrate.WithDropIndex(true),
 		migrate.WithDropColumn(true),
